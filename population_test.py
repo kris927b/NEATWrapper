a2ef2c0357b0791cb16c188c39ec82283dfce94a
krnj <krnj@itu.dk>
2018-10-22 12:07:45 +0200

    Added more tests for population

--- population_test.py
+++ population_test.py
@@ -6,7 +6,21 @@
 
 # Custom libraries
 from population import Population
+from genome import Genome
 
-def test_population():
-    pop = Population(2, 2, 2)
+# ThirdParty Libraries
+import pytest
+
+@pytest.fixture
+def pop():
+    return Population(2, 2, 2)
+
+def test_population(pop):
     assert len(pop.population) == 2
+
+def test_pop_size(pop):
+    assert pop.size() == 2
+
+def test_get_gene(pop):
+    gene = pop.getGene(0)
+    assert type(gene) is Genome
