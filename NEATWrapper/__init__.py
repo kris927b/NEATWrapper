b755b74650534e757a074dde089f6c0a740984e4
krnj <krnj@itu.dk>
2018-10-23 17:44:12 +0200

    Fixed imports

--- NEATWrapper/__init__.py
+++ NEATWrapper/__init__.py
@@ -1,9 +1,9 @@
 print(f'Invoking __init__.py for {__name__}')
-import cartpole
-from connection import Connection
-from genome import Genome
-from innovation import Innovation
-from node import Node
-from population import Population
-import simulation
-from species import Species
+import NEATWrapper.cartpole
+from NEATWrapper.connection import Connection
+from NEATWrapper.genome import Genome
+from NEATWrapper.innovation import Innovation
+from NEATWrapper.node import Node
+from NEATWrapper.population import Population
+import NEATWrapper.simulation
+from NEATWrapper.species import Species
